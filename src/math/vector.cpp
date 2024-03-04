fd7d1bb0d8c54c427c01b10f3a49be4293adcefe
vuavisao <vuavisao18062005@gmail.com>
2024-03-04 21:57:36 +0700

    add basic vector math for future use

---
+++ src/math/vector.cpp
@@ -0,0 +1,41 @@
+#include "math/vector.hpp"
+
+#include <iostream>
+#include <cmath>
+
+Vector::Vector() : 
+	x(0), 
+	y(0)
+{}
+
+Vector::Vector(const Vector& other) :
+	x(other.x), 
+	y(other.y)
+{}
+
+Vector& Vector::operator=(const Vector& other)  {
+	x = other.x;
+	y = other.y;
+	return *this;
+}
+
+Vector::Vector(float x_, float y_) : 
+	x(x_),
+	y(y_)
+{}
+
+Vector Vector::operator+(const Vector& other) const { return Vector(x + other.x, y + other.y); }
+Vector Vector::operator-(const Vector& other) const { return Vector(x - other.x, y - other.y); }
+Vector Vector::operator*(const float& factor) const { return Vector(x * factor, y * factor); }
+Vector Vector::operator/(const float& factor) const { return Vector(x * factor, y * factor); }
+
+Vector& Vector::operator+=(const Vector& other) { *this = (*this) + other; return *this; }
+Vector& Vector::operator-=(const Vector& other) { *this = (*this) + other; return *this; }
+Vector& Vector::operator*=(const float& factor) { *this = (*this) * factor; return *this; }
+
+bool Vector::operator==(const Vector& other) { return (x == other.x && y == other.y); }
+
+Vector Vector::operator-() const { return Vector(-x, -y); }
+
+std::ostream& operator<<(std::ostream& os, const Vector& other) { return os << "(" << other.x << ", " << other.y << ")"; }
+
---
+++ src/math/vector.hpp
@@ -0,0 +1,33 @@
+#ifndef HEADER_ASAED_MATH_VECTOR_MAIN_HPP
+#define HEADER_ASAED_MATH_VECTOR_MAIN_HPP
+
+#include <iosfwd>
+
+class Vector {
+public:
+	float x, y;
+public:
+	Vector();
+	Vector(const Vector& other);
+
+	Vector& operator=(const Vector& other);
+public:
+	Vector(float x_, float y_);
+
+public:
+	Vector operator+(const Vector& other) const;
+	Vector operator-(const Vector& other) const;
+	Vector operator*(const float& factor) const;
+	Vector operator/(const float& factor) const;
+	Vector& operator+=(const Vector& other);
+	Vector& operator-=(const Vector& other);
+	Vector& operator*=(const float& factor);
+
+	bool operator==(const Vector& other);
+	
+	Vector operator-() const;
+
+	friend std::ostream& operator<<(std::ostream& os, const Vector& other);
+}
+
+#endif
